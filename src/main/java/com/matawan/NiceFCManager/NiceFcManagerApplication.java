cc9b51d58d0217bd2db659cf4300381eb1235c40
dyhia <dyhia.bellali.pro@gmail.com>
2023-12-20 16:38:07 +0100

    init socle

---
+++ src/main/java/com/matawan/NiceFCManager/NiceFcManagerApplication.java
@@ -0,0 +1,13 @@
+package com.matawan.NiceFCManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceFcManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceFcManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matawan/NiceFCManager/NiceFcManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matawan.NiceFCManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceFcManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
